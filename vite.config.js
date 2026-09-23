ea2eaa8ed076171a7fcc53475a67f94ea72a5a1e
Ivan <borisenkovania@gmail.com>
2026-09-23 13:52:35 +0300

    изменения

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', 
+        target: 'https://speakers-leon-solar-lucas.trycloudflare.com',
         changeOrigin: true,
       }
     }
